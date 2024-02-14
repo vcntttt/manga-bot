6c50e147e9294306f15b8175a4a70460826b8a01
Vicente Rivera <vicenterivera225@gmail.com>
2024-02-14 20:13:26 -0300

    turn into discord bot

--- main.py
+++ main.py
@@ -4,50 +4,65 @@ import re
 import csv
 from discord.ext import commands, tasks
 from bs4 import BeautifulSoup
-from env import BOT_TOKEN
+from env import BOT_TOKEN, CHANNEL_ID
 
 intents = discord.Intents.default()
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="&", intents=intents)
 headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
     }
 patron = r'Capítulo (\d+)'
 
-mangas = []
-with open("mangas.csv", "r", encoding="utf-8") as file:
-  reader = csv.DictReader(file)
-  for row in reader:
-     mangas.append({
+def loadCSV():
+  mangas = []
+  with open ('mangas.csv','r',encoding='utf-8') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+      mangas.append({
         'manga': row['manga'],
         'url': row['url'],
-        'ultimoCapRegistrado': int(row['ultimoCapRegistrado'])
-     })
+        'lastCapRegistered': int(row['lastCapRegistered']),
+      })
+  return mangas
 
-def checkCap(manga, lastCapRegistered):
-    response = requests.get(manga['url'], headers=headers)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    lastCapElement = soup.find('a', class_='btn-collapse')
-    lastCapText = lastCapElement.text
-    match = re.search(patron, lastCapText)
-    lastCap = match.group(1) if match else "Número de capítulo no encontrado"
-    return manga['manga'] + ': ' + lastCap if int(lastCap) != int(lastCapRegistered) else manga['manga'] + ': ' + 'No hay nuevos capítulos'
-
-if __name__ == "__main__":
+async def checkCap(manga):
+  response = requests.get(manga['url'], headers=headers)
+  soup = BeautifulSoup(response.text, 'html.parser')
+  lastCapElement = soup.find('a', class_='btn-collapse')
+  if lastCapElement and lastCapElement.text:
+    lastCap = lastCapElement.text
+    match = re.search(patron, lastCap)
+    return int(match.group(1)) if match else None
+  
+def updateCSV(mangas):
+  with open('mangas.csv', 'w', newline='', encoding='utf-8') as file:
+    fieldnames = ['manga', 'url', 'lastCapRegistered']
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+    writer.writeheader()
     for manga in mangas:
-        print(checkCap(manga, manga['ultimoCapRegistrado']))
+      writer.writerow(manga)
 
-# @tasks.loop(minutes=1)
-# async def scheduledCheck(ctx):
-#   channel = bot.get_channel(1109648180297089065)
-#   if channel:
-#     for url in mangas:
-#       ultimoCapRegistrado = ultimocap[url]
-#       lastCapText = checkCap(url, ultimoCapRegistrado)
-#       await channel.send(f'Último capítulo de {url}: {lastCapText}, registrado: {ultimoCapRegistrado}')
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+    scheduledCheck.start()
 
-# @scheduledCheck.before_loop
-# async def before_scheduledCheck():
-#     print('Esperando a que el bot esté listo...')
-#     await bot.wait_until_ready()
+@tasks.loop(minutes=1)
+async def scheduledCheck():
+  channel = bot.get_channel(int(CHANNEL_ID))
+  if channel:
+    print('Checking...')
+    await channel.send('Checking...')
+    mangas = loadCSV()
+    for manga in mangas:
+      lastCap = await checkCap(manga)
+      if lastCap and lastCap > manga['lastCapRegistered']:
+        await channel.send(f'Nuevo capitulo de {manga["manga"]}: {lastCap}')
+        manga['lastCapRegistered'] = lastCap
+      else: 
+        await channel.send('no cap')
+      updateCSV(mangas)
 
-# bot.run(BOT_TOKEN)
+
+
+bot.run(BOT_TOKEN)
